c6974e0127e819a0030283145259b888ddc6add2
Sherlock <sherlock@gmail.com>
2018-05-08 15:38:09 +0630

    Created message component and edited chat.blade.php

---
+++ resources/assets/js/components/message.vue
@@ -0,0 +1,15 @@
+<template>
+    <li class="list-group-item"><slot></slot></li>
+</template>
+
+<script>
+
+    export default {
+
+    }
+
+</script>
+
+<style>
+
+</style>
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -8,6 +8,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Chat</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+        .list-group {
+            overflow-y: auto;
+            height: 200px;
+        }
+    </style>
 </head>
 <body>
 
@@ -15,13 +21,13 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-4">
+                <li class="list-group-item active">Chat Room</li>
                 <ul class="list-group">
-                    <li class="list-group-item active">Chat Room</li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
-                    <li class="list-group-item">Morbi leo risus</li>
-                    <li class="list-group-item">Porta ac consectetur ac</li>
-                    <input type="text" class="form-control" placeholder="Type your message..">
+                    <message v-for="value in chat.message">
+                        @{{ value }}
+                    </message>
                 </ul>
+                <input type="text" class="form-control" placeholder="Type your message.." v-model="message" v-on:keyup.enter="send">
             </div>
         </div>
     </div>
